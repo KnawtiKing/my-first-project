bc5d42ae6d9cda66e6267f64f1228a26ddadd259
Ivey Nixon <Hardstyleteddy25@gmail.com>
2025-08-23 18:58:37 -0700

    chore: add VS Code autopep8 setup and sample files

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello, VS Code") 
+import os
+
+print(f"My Working Directory is: {os.getcwd()}")
+
+x = 10
+if x % 2 == 0:
+    print(f"{x} is a even number")
+else:
+    print(f"{x} is a odd number")
